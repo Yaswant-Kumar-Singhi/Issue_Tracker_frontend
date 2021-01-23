500e04f159b3e82f7bf91a2c7caf83ce82009b0e
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2021-01-24 02:29:03 +0530

    adding antd for design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import {Layout , Menu} from 'antd' 
+import 'antd/dist/antd.css'
+import {Link} from 'react-router-dom'
 
-const Home = ()=> {
+
+const {Header , Content , Footer} = Layout
+
+const Home = () =>{
+
 
 
     return(
-        <div>
-            Home page
-        </div>
+        <Layout>
+            <Header>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']}>
+                    <Menu.Item key="home">
+                        <Link to="/"> Home</Link>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+
+            <Content style={{"padding":"25px 50px","minHeight":"550px"}}>
+                Project card
+            </Content>
+            <Footer style={{"textAlign":"center"}}>
+                <p>@Made with Love</p>
+            </Footer>
+        </Layout>
     )
+
 }
 
 export default Home;
