67f9a666a8dbb52365b57bf2b5a4ca8eb1f66e22
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2021-02-02 18:28:02 +0530

    successfully listed all issues

--- src/pages/issue/IssueList.js
+++ src/pages/issue/IssueList.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderComponent from '../../components/card/HeaderComponent'
+import {getIssues} from '../../functions/project_function'
 
 const IssueList = () => {
+
+    const [issues , setIssues ] = useState([])
+
+    useEffect(()=>{
+        loadAllIssues();
+    },[])
+
+    const loadAllIssues = () =>{
+        
+        getIssues().then((res)=>{
+            setIssues(res.data)
+            
+        })
+    }
 
     return(
         <div>
             <HeaderComponent />
             I am in add issue page
             <br />
+            <div className="container">
+                    <div className="row">
+                        {issues.map((issue)=>(
+                            <div key={issue._id} className="col-md-3 pb-3">                    
+                               {issue.title}
+                            </div>))}
+                    </div>
+                </div>
+
             
         </div>
     )
